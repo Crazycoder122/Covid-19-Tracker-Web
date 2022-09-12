d7ff8ed36424c4b7f013a7398a846b23a042b458
ArneshRC <84434238+ArneshRC@users.noreply.github.com>
2022-09-12 14:32:01 +0530

    added new searching and filtering logic

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const countryData = document.querySelector('.country-data');
-const basisOfSorting = document.querySelector('.basis-of-sorting');
-const worldData = {
+const countryDataTable = document.querySelector('.country-data');
+const worldDataCards = {
     infected: document.querySelector('.card-text-infected'),
     recovered: document.querySelector('.card-text-recovered'),
     deceased: document.querySelector('.card-text-deceased')
 }
 const countryStatsUrl = "https://disease.sh/v3/covid-19/countries";
 const worldStatsUrl = "https://disease.sh/v3/covid-19/all";
+const statusDiv = document.querySelector('.status');
+const searchBar = document.querySelector('.search');
+const searchClearButton = document.querySelector('.button-clear');
+const tableHeaders = [...document.querySelectorAll('th')];
+
+function getRows() {
+    const tbody = countryDataTable.children[0];
+    return [...tbody.children].slice(1)
+}
 
 function addRow(flagUrl, country, infected, recovered, deceased) {
 
@@ -32,20 +40,20 @@ function addRow(flagUrl, country, infected, recovered, deceased) {
     cells.deceased.classList.add('deceased')
     cells.deceased.textContent = deceased;
 
-    for(const key in cells)
-        row.appendChild(cells[key]);
+    for(const idx in cells)
+        row.appendChild(cells[idx]);
 
-    countryData.children[0].appendChild(row);
+    countryDataTable.children[0].appendChild(row);
 }
 
-function updateCountryData() {
-    while(countryData.children[0].children.length != 1)
-        countryData.children[0].removeChild(
-            countryData.children[0].children[
-                countryData.children[0].children.length - 1
-            ]
+function deleteAllRows() {
+    while(countryDataTable.children[0].children.length != 1)
+        countryDataTable.children[0].removeChild(
+            countryDataTable.children[0].lastChild
         );
-    
+}
+
+function fetchCountryData() {
     fetch(countryStatsUrl)
     .then(response => response.json())
     .then(data => {
@@ -61,15 +69,99 @@ function updateCountryData() {
     });
 }
 
-function updateWorldData() {
+function fetchWorldData() {
     fetch(worldStatsUrl)
     .then(response => response.json())
     .then(data => {
-        worldData.infected.textContent = data.cases;
-        worldData.recovered.textContent = data.recovered;
-        worldData.deceased.textContent = data.deaths;
+        worldDataCards.infected.textContent = data.cases;
+        worldDataCards.recovered.textContent = data.recovered;
+        worldDataCards.deceased.textContent = data.deaths;
     });
 }
 
-updateCountryData();
-updateWorldData();
+function updateStatus() {
+    const sortedHeader = document.querySelector('[data-sorted="true"]');
+
+    const notFound = getRows().every(tr => tr.hidden);
+
+    if(notFound) {
+        statusDiv.textContent = "There are no countries matching your search query."
+        return;
+    }
+
+    const sortingKey = sortedHeader.dataset.content;
+    const isAscending = sortedHeader.dataset.asc === 'true';
+    const isNumeric = sortingKey !== 'country';
+    const hasFilter = searchBar.value !== '';
+
+    statusDiv.textContent = "The list is sorted in";
+    statusDiv.textContent += (isAscending ? " ascending" : " descending");
+    statusDiv.textContent += (isNumeric ? " numeric" : " alphabetical");
+    statusDiv.textContent += " order of";
+    switch(sortingKey) {
+        case 'country':
+            statusDiv.textContent += " country names";
+            break;
+        case 'infected':
+            statusDiv.textContent += " infected cases";
+            break;
+        case 'recovered':
+            statusDiv.textContent += " recovered cases";
+            break;
+        case 'deceased':
+            statusDiv.textContent += " deceased cases";
+            break;
+    }
+
+    if(hasFilter)
+        statusDiv.textContent += " and filtered according to your search query"
+
+    statusDiv.textContent += ".";
+
+}
+
+fetchCountryData();
+fetchWorldData();
+updateStatus();
+
+searchClearButton.addEventListener('click', _ev => {
+    searchBar.value = '';
+    trs = getRows();
+    trs.forEach(tr => {
+        tr.hidden = false
+    });
+});
+
+searchBar.addEventListener('input', _ev => {
+    trs = getRows();
+    trs.forEach(tr => {
+        lowerCaseCountryName = tr.children[0].textContent.toLowerCase();
+        lowerCaseSearchQuery = searchBar.value.toLowerCase();
+        tr.hidden = !lowerCaseCountryName.includes(lowerCaseSearchQuery)
+    });
+    updateStatus();
+});
+
+for(const idx in tableHeaders) {
+    const th = tableHeaders[idx];
+    th.addEventListener('click', _ev => {
+        trs = getRows();
+        const isNumeric = th.dataset.content != 'country';
+        const isAscending = th.dataset.asc == 'true';
+        const isSorted = th.dataset.sorted == 'true';
+        trs.sort((a, b) => {
+            if(isAscending && isSorted) [a, b] = [b, a];
+            return a.children[idx].textContent.localeCompare(
+                b.children[idx].textContent, undefined,
+                { numeric: isNumeric }
+            )
+        });
+        for(const tr of trs)
+            countryDataTable.children[0].appendChild(tr);
+        th.dataset.asc = String(!isAscending || !isSorted);
+        tableHeaders.forEach(thElem => {
+            thElem.dataset.sorted = String(thElem === th);
+        });
+        updateStatus();
+    });
+}
